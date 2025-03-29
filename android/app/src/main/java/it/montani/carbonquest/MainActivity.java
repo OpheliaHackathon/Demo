658c1ffd04b00ebec737585433ca2598b3eb46cf
Lorenzo <hello@lorenzo0111.me>
2025-03-29 05:12:51 +0100

    Aggiunto capacitor

---
+++ android/app/src/main/java/it/montani/carbonquest/MainActivity.java
@@ -0,0 +1,5 @@
+package it.montani.carbonquest;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "it.montani.carbonquest",
+  appName: "CarbonQuest",
+  webDir: "dist",
+};
+
+export default config;
