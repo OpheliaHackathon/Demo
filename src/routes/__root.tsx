027dcdb5588e0389d6e5c5be00b4afa45147456c
Lorenzo <hello@lorenzo0111.me>
2025-03-29 05:29:13 +0100

    Aggiunto controllo mobile

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,27 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { Capacitor } from "@capacitor/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
-  component: () => (
+  component: Root,
+});
+
+function Root() {
+  const navigate = Route.useNavigate();
+  if (Capacitor.getPlatform() !== "web" && location.pathname === "/") {
+    navigate({
+      to: "/dashboard",
+    });
+    return null;
+  }
+
+  return (
     <QueryClientProvider client={queryClient}>
       <div className="flex h-screen">
         <Outlet />
       </div>
     </QueryClientProvider>
-  ),
-});
+  );
+}
